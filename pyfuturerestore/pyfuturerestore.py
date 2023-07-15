bd28b489d6cbe4195cf6c3d38a5d4e811810103d
Mini-Exploit <miniexploitttt@gmail.com>
2023-07-14 21:55:26 -0500

    reboot after setting nonce

--- pyfuturerestore/pyfuturerestore.py
+++ pyfuturerestore/pyfuturerestore.py
@@ -758,6 +758,7 @@ class PyFuturerestore:
             self.logger.info('Done setting nonce!')
             self.logger.info('Use futurerestore --exit-recovery to go back to normal mode if you aren\'t restoring.')
             self.irecv.set_autoboot(False)
+            self.irecv.reboot()
             sys.exit(0)
         sleep(2)
 
